199fb1632c623628840e08de1aebd385f3ac0d1f
sahars03 <sahar.sohail@warwick.ac.uk>
2025-09-04 19:01:22 +0100

    Added schema

---
+++ src/app/schema.sql
@@ -0,0 +1,26 @@
+/* CREATE DATABASE job_tracker;
+
+\c job_tracker */
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(100) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE applications (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  job_title VARCHAR(255) NOT NULL,
+  company VARCHAR(255) NOT NULL,
+  job_location VARCHAR(255) NOT NULL,
+  job_type VARCHAR(50) NOT NULL,
+  work_setting VARCHAR(255)[] NOT NULL,
+  date_applied DATE NOT NULL,
+  app_status VARCHAR(50),
+  stage_reached VARCHAR(100),
+  notes TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
