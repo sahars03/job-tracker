7ae114b2ab925b950716934d4c0f188723a4e46a
sahars03 <sahar.sohail@warwick.ac.uk>
2025-12-11 17:05:18 +0000

    Started making login page functional

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,46 @@
-import Link from "next/link"
+import Link from "next/link";
+import { useState } from "react";
 
 export default function LoginPage() {
+
+  const [formData, setFormData] = useState({
+    un_email: "",
+    password: "",
+  });
+
+  const [formError, setFormError] = useState(false);
+  const [pwError, setPwError] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // update the form inputs every time they are changed
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const validatePassword = () => {
+    // TODO: replace this logic with checking at the backend if the login is correct
+    return formData.password == formData.password;
+  };
+
+  // validates form data by checking if all the required fields have been filled in
+  const validateForm = () => {
+    // list of required fields
+    const required = [
+      formData.un_email,
+      formData.password
+    ];
+    
+    // check if all required fields are filled
+    return required.every(field => field !== "") && validatePassword();
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setFormError(false);
+    setPwError(false);
+    // TODO: validate input and redirect the user to their dashboard (maybe the homepage for now?)
+  };
+
   return (
     <div className="font-sans flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20">
       <p className="font-sans text-6xl text-center sm:text-left">Log in</p>
@@ -8,8 +48,8 @@ export default function LoginPage() {
       <p className="mb-6 text-xl">Fill out the form below to log in.</p>
       <form className="flex flex-col gap-4 w-full items-center justify-center max-w-md">
         <div className="mb-4 flex flex-col gap-2 w-3/4">
-          <input type="text" id="name-email" className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200" placeholder="Username/email"/>
-          <input type="text" id="password" className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200" placeholder="Password"/>
+          <input type="text" id="un_email" onChange={handleChange} className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200" placeholder="Username/email"/>
+          <input type="text" id="password" onChange={handleChange} className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200" placeholder="Password"/>
         </div>
       </form>
       <Link href="/mainpage">
