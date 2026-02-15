2608ee1a2f9d236920929510846fb02ecc4d8030
sahars03 <sahar.sohail@warwick.ac.uk>
2026-02-15 14:22:38 +0000

    Updated

--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -1,23 +1,15 @@
 import dotenv from "dotenv"
 
-
-// THIS CODE WILL BE FOR ADDING AN APPLICATION TO A USER'S ACCOUNT
-
-
-
 dotenv.config();
 
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 
-// POST /api/form
 router.post("/", async (req, res) => {
   try {
-    // update this with the other fields
     const { name, email, message } = req.body;
 
-    // update this with the other fields
     const result = await pool.query(
       `INSERT INTO ${process.env.PG_DATABASE} (name, email, message)
        VALUES ($1, $2, $3)
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-// THIS WILL HELP WITH ADDING AN APPLICATION TO A USER'S ACCOUNT
-
 import express from "express";
 import cors from "cors";
 
@@ -14,7 +12,7 @@ app.use(express.json());
 // routes
 app.use("/api/form", formRoutes);
 
-// start server
+// server
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
